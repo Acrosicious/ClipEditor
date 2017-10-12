78c302bce51cea74dd599535584f131cfe035ec7
Sebastian <basti778@gmx.net>
2017-10-12 12:35:23 +0200

    Projektdateien hinzufügen.

---
+++ VideoClipEditor/MoveToPoint_AndDrag_Slider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VideoClipEditor
+{
+    public class MoveToPoint_AndDrag_Slider : Slider
+    {
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //OnPreviewMouseLeftButtonDown(new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left));
+                OnThumbDragStarted(new System.Windows.Controls.Primitives.DragStartedEventArgs(0, 0));
+            }
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //OnPreviewMouseLeftButtonDown(new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left));
+                OnThumbDragStarted(new System.Windows.Controls.Primitives.DragStartedEventArgs(0, 0));
+                //Console.WriteLine("Enter method");
+                
+            }
+        }
+    }
+}
---
+++ VideoClipEditor/OkDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VideoClipEditor
+{
+    /// <summary>
+    /// Interaktionslogik für OkDialog.xaml
+    /// </summary>
+    public partial class OkDialog
+    {
+
+        private string file;
+
+        public OkDialog(string file)
+        {
+            this.file = file;
+
+            InitializeComponent();
+            this.Owner = App.Current.MainWindow;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(file))
+            {
+                System.Diagnostics.Process.Start("explorer.exe", "/Select,\"" + file + "\"");
+            }
+
+            this.Close();
+        }
+    }
+}
